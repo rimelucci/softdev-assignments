78ac1c23ed7fc919028fc9aa66fed7a2f8536c29
Rick Melucci <rimelucci@gmail.com>
2016-03-21 23:27:36 -0400

    most transitions work now

--- hw05/index.js
+++ hw05/index.js
@@ -12,7 +12,7 @@ var repstate = ["California", "Texas", "Florida", "New York", "Georgia", "North 
 
 //Ricks
 var democratbtn = document.getElementById('democrats');
-var republicanbtn = document.getElementById('republicans');
+var republicanbtn = document.getElementById('republican');
 var democratNumber = [548, 291, 251, 246, 210, 182, 160, 147, 142, 121, 118, 118, 117, 116, 109, 96, 93, 92, 85, 84, 78, 75, 74, 71, 67, 60, 59, 59, 52, 46, 43, 43, 42, 41, 37, 37, 37, 37, 35, 33, 32, 31, 30, 30, 27, 27, 26, 25, 23, 20, 18, 17, 12, 12, 11, 11, 1];
 
 var democratState = ["California", "New York", "Texas", "Florida", "Pennsylvania", "Illinois", "Ohio", "Michigan", "New Jersey", "North Carolina", "Maryland", "Washington", "Georgia", "Massachussets", "Virginia", "Wisconsin", "Minessota", "Indiana", "Arizona", "Missouri", "Colorado", "Tennessee", "Oregon", "Connecticut", "Puerto Rico", "Alabama", "Kentucky", "Louisiana", "South Carolina", "Iowa", "District of Columbia", "Nevada", "New Mexico", "Oklahoma", "Mississippi", "Arkansas", "Kansas", "Utah", "West Virginia", "Hawaii", "Rhode Island", "New Hampshire", "Delaware", "Maine", "Nebraska", "Idaho", "Montana", "Vermont", "South Dakota", "North Dakota", "Alaska", "Wyoming", "Democrats Abroad", "Guam", "Virgin Islands", "American Samoa", "Northern Marianas", "Unassigned"];
@@ -31,7 +31,7 @@ var create_graph = function(){
 		rkeychart();
 		rkeychart2();
 		d3chart(repnum, repstate, repalloted);
-		democratD3chart(democratNumber);
+		//democratD3chart(democratNumber);
 };
 
 var rkeychart = function rkeychart() {
@@ -84,6 +84,8 @@ var d3chart = function(data, state, allot){
 				.data(data)
 				.enter()
 				.append("div")
+				.transition()
+				.duration(750)
 				.style("width", function(d)
 							 {return d*4 + "px"; })
 		// Add allotment coloring
@@ -108,6 +110,8 @@ var democratD3chart = function(data) {
 				.data(data)
 				.enter()
 				.append("div")
+				.transition()
+				.duration(750)
 				.style("width", function(d){
 						return d*2 + "px";
 				})
@@ -125,12 +129,20 @@ democratbtn.addEventListener("click", function() {
     d3.select(".chart")
       .selectAll("div")
       .transition()
-      .duration(500)
-      .style("width", "0");
+      .duration(750)
+      .style("width", "0")
+			.remove();
+		//democratD3chart(democratNumber);
 });
 
 republicanbtn.addEventListener("click", function() {
-
+	d3.select(".chart")
+		.selectAll("div")
+		.transition()
+		.duration(750)
+		.style("width", "0")
+		.remove();
+	//d3chart(repnum, repstate, repalloted);
 });
 
 create_graph();
