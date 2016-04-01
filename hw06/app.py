d5e230834f64cdcee3b9a85a6e8578072c50c082
Rick Melucci <rimelucci@gmail.com>
2016-04-01 00:58:55 -0400

    added the python lab

---
+++ hw06/app.py
@@ -0,0 +1,54 @@
+#nums = [ x for x in range(8)]
+#print nums
+
+#nums = [ x*x for x in range(8)]
+#print nums
+
+#nums = [(x, x*x, x*x*x) for x in range(8)]
+#print nums
+
+p = "BurgersAndTangs1234"
+f = "thispasswordsucks"
+d = "ShOrT"
+UC_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LC_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+numbers = "1234567890"
+symbols = ".?!&#,;:-_*"
+
+#YOUR TASK The First:
+#Write a function that uses list comprehension to return whether a password meets a minimum threshold: it contains a mixture of upper- and lowercase letters, and at least one number.
+def passwordChecker( string ):
+    upperCase =  [ 1 if x in UC_LETTERS else 0 for x in string]
+    lowerCase = [ 1 if x in LC_LETTERS else 0 for x in string]
+    numbersCheck = [1 if x in numbers else 0 for x in string]
+    #print upperCase
+    #print lowerCase
+    #print numbersCheck
+    if (sum(upperCase) > 0 and sum (lowerCase) > 0 and sum(numbersCheck) > 0):
+        return True
+
+    return False
+
+print passwordChecker(p)
+print passwordChecker(f)
+
+#YOUR TASK The Second:
+#Write a function that uses list comprehension to return a password's strength rating. This function should return a low integer for a weak password and a higher integer for a stronger password. (Suggested scale: 1-10)
+#Consider these criteria:
+#* mixture of upper- and lower-case
+#* inclusion of numerals
+#* inclusion of these non-alphanumeric chars: . ? ! & # , ; : - _ *
+
+def brolicPasswordirizer( string ):
+    upperCase =  [ 1 if x in UC_LETTERS else 0 for x in string]
+    lowerCase = [ 1 if x in LC_LETTERS else 0 for x in string]
+    numbersCheck = [1 if x in numbers else 0 for x in string]
+    symbolsCheck = [1 if x in symbols else 0 for x in string]
+
+    totalCount = upperCase.count(1) * 2 + lowerCase.count(1) + numbersCheck.count(1) * 2 + symbolsCheck.count(1) * 4
+
+    print totalCount / 3
+
+brolicPasswordirizer( p )
+brolicPasswordirizer( f )
+brolicPasswordirizer( d )
